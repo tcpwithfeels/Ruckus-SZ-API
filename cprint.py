ec89ca7d77c09d5e32c1d98f6ae9e81c36047f09
= <=>
2023-03-23 09:36:41 +1100

    cbr

---
+++ cprint.py
@@ -0,0 +1,30 @@
+"""
+Written by Craig B.
+https://github.com/Phatkone
+           ,,,
+          (. .)
+-------ooO-(_)-Ooo-------
+
+"""
+
+def cprint(string: str, type: str = "endc", bold: bool = False, underline: bool = False) -> None:
+    colours = {
+        'PURPLE': '\033[95m',
+        'BLUE': '\033[94m',
+        'CYAN': '\033[96m',
+        'GREEN': '\033[92m',
+        'YELLOW': '\033[93m',
+        'RED': '\033[91m',
+        'ENDC': '\033[0m',
+        'BOLD': '\033[1m',
+        'UNDERLINE': '\033[4m',
+    }
+    if type.upper() in ['PURPLE','BLUE','CYAN','GREEN','YELLOW','RED','BOLD','UNDERLINE']:
+        s = colours[type.upper()]
+        if bold:
+            s = s + colours['BOLD']
+        if underline:
+            s = s + colours['UNDERLINE']
+        print("{}{}{}".format(colours[type.upper()],string,colours['ENDC']))
+    else:
+        print(string)
---
+++ ruckus_AP_add.py
@@ -0,0 +1,76 @@
+# Python 
+
+import requests
+import json
+import cprint
+
+class ruckus_SZ_API:
+
+    def __init__(self, host: str):
+        self.prefix_pattern = "https://{}:8443/wsg/api/public".format(host)
+
+    def retrieve_api_version(self):
+        
+        # Get the API Version
+        
+        url = "{}/apiInfo".format(self.prefix_pattern)
+        api_version = requests.get(url)
+
+        return api_version
+
+
+
+    def ruckus_login(self, api_version, username, password):
+
+        url = "{}/{}/serviceTicket".format(self.prefix_pattern, api_version)
+
+        # Request Body in POST
+        # Required [username, password]
+
+        request_body = {
+            "username": username,
+            "password": password
+        }
+        try:
+            requests.post(request_body, data=request_body, verify=verify)
+        except requests.exceptions.ConnectionError as e:
+            cprint("Unable to establish session to Ruckus Smart Zone:\n  ", 'red', True)
+            cprint(e,'yellow')
+            exit()
+        return session
+
+
+
+
+
+    def retrieve_zone_id(self, session, zone_name):
+        url = ""
+        return zone_id
+
+    def retrieve_group_id(self, session, zone_id, group_name):
+
+        return group_id
+
+    def create_ruckus_ap(self, name, mac, zone_id, group_id):
+    # "required" : [ "mac", "zoneId" ]
+        return status_code
+
+def main():
+    # Main HOST Variable for API Call
+    host = "192.X.X.X"
+    
+    # Ruckus Session 
+    ruckus_sesh = ruckus_SZ_API(host)
+
+    # API Version needed for log on
+    api_version = ruckus_sesh.retrieve_api_version()
+
+    
+    
+
+    ruckus_login(host, api_version, username, password)
+
+
+if __name__ == '__main__':
+    main()
+
